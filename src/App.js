18903658ae0babf796670088d8065128ffef2fe6
dinnf <finn.denis@gmail.com>
2023-02-15 13:59:03 +1300

    endgame finished

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import Die from './components/Die'
 import {useState,useEffect} from 'react'
 import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
 
 function App() {
   const [newRoll, setNewRoll] = useState(allNewDice())
   const [tenzies, setTenzies] = useState(false)
-  useEffect(()=>{if (newRoll.every(newRoll.isHeld)){setTenzies(true)}},[newRoll])
+  useEffect(()=>{
+    let allDiceHeld = newRoll.every((die) => {return die.isHeld})
+    let firstDice = newRoll[0].value
+    let allDiceSame = newRoll.every((die) => {return die.value === firstDice})
+    if (allDiceHeld && allDiceSame) {setTenzies(true)}
+  },[newRoll])
   function allNewDice() {
    let randomNumberArray = []
     for (let i = 1; i<11; i++) {
@@ -17,9 +23,13 @@ const newDiceObject = randomNumberArray.map(number => {return {value: number, is
 console.log(`newDiceObject: ${newDiceObject}`) 
 return newDiceObject}
 function refreshRoll() {
+  if (tenzies) {
+    setTenzies(false)
+    setNewRoll(allNewDice())}
+  else {
   setNewRoll(oldDice => oldDice.map(die => {
     return die.isHeld ? die : {value:Math.ceil(Math.random() *6),id: nanoid(), isHeld: false}
-  }))
+  }))}
 }
 
 
@@ -33,13 +43,14 @@ function holdDice(id) {
 const dieElements = newRoll.map(element => <Die isHeld = {element.isHeld} key={element.id} value={element.value} holdDice={() =>holdDice(element.id)}/>)
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="die-container">
           {dieElements}
         </div>
         <div>
-          <button className="roll-dice" onClick={() => {refreshRoll()}}>Roll Dice</button>
+          <button className="roll-dice" onClick={() => {refreshRoll()}}>{tenzies ? "Play again" : "Roll Dice"}</button>
           
         </div>
     </main>
